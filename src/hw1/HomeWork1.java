2542e80ff43e9ba8a761164d9ef7bfa42babcffd
Yang <yang88991116@gmail.com>
2024-08-22 16:28:45 +0800

    HW finshed

---
+++ src/hw1/HomeWork1.java
@@ -0,0 +1,45 @@
+package hw1;
+
+public class HomeWork1
+{
+	public static void main(String args[])
+	{
+//1.題目一
+		int a=12,b=6,c=a+b,d=a*b;
+			System.out.println("12+6="+c);
+			System.out.println("12*6="+d);
+			System.out.println();
+		
+//2.題目二		
+		int egg=200;
+			System.out.println("200顆蛋有"+egg/12+"打"+egg%12+"顆");
+			System.out.println();
+		
+//3.題目三		
+		int time=256559,day=time/86400,
+			hour=time%86400/3600,
+			minute=time%86400%3600/60,
+			second=time%86400%3600%60;	
+			System.out.println(day+"天"+hour+"小時"+minute+"分"+second+"秒");
+			System.out.println();
+
+//4.題目四	
+		final double pi=3.1415;
+		int r=5;
+				System.out.println("圓面積="+pi*r*r);
+				System.out.println("圓周長="+pi*2*r);
+				System.out.println();
+//5.題目五				
+		double money10=150*Math.pow((1+0.02),10);				
+				System.out.println("10年後有"+money10+"萬");
+				System.out.println();
+	
+//6.題目六		
+		System.out.println(5+5);
+		//這裏是數值相加
+		System.out.println(5+'5');
+		//因爲'5'的unicode編碼為0035 轉爲十進制為55 所以相加為58
+		System.out.println(5+"5");
+		//這裏是字串連接
+	}
+}
---
+++ src/hw2/HomeWork2.java
@@ -0,0 +1,103 @@
+package hw2;
+
+public class HomeWork2 
+{
+	public static void main(String args[])
+	{
+//1.題目一
+		int sum1=0;
+		for(int i=1; i<=1000; i++)
+		{
+			if(i%2 == 0)
+			sum1 += i;
+		}
+		System.out.println(sum1+"\n");
+		
+//2.題目二
+		int sum2=1;
+		for(int i=1; i<=10; i++)
+		{
+			sum2 *= i;
+		}
+		System.out.println(sum2+"\n");
+				
+//3.題目三
+		int sum3=1,i=1;
+		while(i<=10)
+		{
+			sum3 *= i;
+			i++;
+		}
+		System.out.println(sum3+"\n");
+
+//4.題目四
+		for(int j=1; j<=10; j++)
+			System.out.print(j*j+" ");
+		System.out.println("\n");
+		
+		
+//5.題目五
+		int count=0;
+		for(int j=1; j<=49; j++)
+		{
+			if(j==4 || j%10==4 || j/10==4)
+				continue;
+			System.out.print(j+" ");
+			count++;
+		}
+		System.out.println();
+		System.out.println("總共有："+count+"個"+"\n");
+		
+//6.題目六	
+		for(int j=10; j>=1; j--)
+		{
+			int z=1;
+			while(z<=j)
+				{
+					System.out.print(z+" ");
+					z++;
+				}
+			System.out.println();
+		}
+		System.out.println();
+		
+//7.題目七
+		char z='A';
+		for(int j=0; j<=5; j++)
+		{
+			int k=0;
+			while(k<=j)
+				{
+					System.out.print((char)(z+j));
+					k++;
+				}
+			System.out.println();
+		}
+		System.out.println();
+			
+//8.for + do while
+		for(int o=1; o<=9; o++)
+		{
+			int j=1;
+			do{
+			System.out.print(o+"*"+j+"="+o*j+"\t");
+			j++;		
+			}while(j<10);
+			System.out.println();
+		}
+		System.out.println();
+		
+//9.while + do while
+		int p=1;
+		while(p<10)
+		{
+			int j=1;
+			do{
+			System.out.print(p+"*"+j+"="+p*j+"\t");
+			j++;		
+			}while(j<10);
+			System.out.println();
+			p++;
+		}	
+	}	
+}
